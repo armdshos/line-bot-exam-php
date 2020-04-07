c43d353e914d783533feb36e3a11d1e0e128e9ca
armdshos <arm.panu@gmail.com>
2020-04-07 13:23:15 +0700

    Update botpush_image.php

--- botpush_image.php
+++ botpush_image.php
@@ -16,7 +16,7 @@ $accessToken = "IKtQS7iWe72sF7u0/a6PpFVxuuiXnZ0Nc864Q8wd+U7dRaqpMjLeRXk/8A1QWQAH
       $arrayPostData['messages'][0]['text'] = "รูปโลโก้";
       $arrayPostData['messages'][1]['type'] = "image";
       $arrayPostData['messages'][1]['originalContentUrl'] = "https://youant.net/green/images/48412313_558355137924389_6011670775092740096_n.jpg";
-      //$arrayPostData['messages'][1]['stickerId'] = "35";
+      $arrayPostData['messages'][1]['previewImageUrl'] = "https://youant.net/green/images/48412313_558355137924389_6011670775092740096_n.jpg";
       pushMsg($arrayHeader,$arrayPostData);
    //}
    function pushMsg($arrayHeader,$arrayPostData){

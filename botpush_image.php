3b5257c07a4858884d07c3e57e854f0c7bfc8bcb
armdshos <arm.panu@gmail.com>
2020-04-07 13:20:49 +0700

    Update botpush_image.php

--- botpush_image.php
+++ botpush_image.php
@@ -1,25 +1,37 @@
 <?php
+$accessToken = "IKtQS7iWe72sF7u0/a6PpFVxuuiXnZ0Nc864Q8wd+U7dRaqpMjLeRXk/8A1QWQAHQLqPuTQj2XC8hWvjJDV5XMvtIlClUpSj0DPKuepgqJsWHwRFl9BHBrvxP6g9PVWnVJyMra6tTGOZK9wWrPP0rwdB04t89/1O/w1cDnyilFU=";
+   //$content = file_get_contents('php://input');
+   //$arrayJson = json_decode($content, true);
+   $arrayHeader = array();
+   $arrayHeader[] = "Content-Type: application/json";
+   $arrayHeader[] = "Authorization: Bearer {$accessToken}";
+   //รับข้อความจากผู้ใช้
+   $message = 'สวัสดี';//$arrayJson['events'][0]['message']['text'];
+   //รับ id ของผู้ใช้
+   $id = 'Ud4d744beed535713bb7f605ca0e1eba6';//$arrayJson['events'][0]['source']['userId'];
+   #ตัวอย่าง Message Type "Text + Sticker"
+   //if($message == "สวัสดี"){
+      $arrayPostData['to'] = $id;
+      $arrayPostData['messages'][0]['type'] = "image";
+      $arrayPostData['messages'][0]['originalContentUrl'] = "https://youant.net/green/images/48412313_558355137924389_6011670775092740096_n.jpg";
+      //$arrayPostData['messages'][1]['type'] = "sticker";
+      //$arrayPostData['messages'][1]['packageId'] = "2";
+      //$arrayPostData['messages'][1]['stickerId'] = "35";
+      pushMsg($arrayHeader,$arrayPostData);
+   //}
+   function pushMsg($arrayHeader,$arrayPostData){
+      $strUrl = "https://api.line.me/v2/bot/message/push";
+      $ch = curl_init();
+      curl_setopt($ch, CURLOPT_URL,$strUrl);
+      curl_setopt($ch, CURLOPT_HEADER, false);
+      curl_setopt($ch, CURLOPT_POST, true);
+      curl_setopt($ch, CURLOPT_HTTPHEADER, $arrayHeader);
+      curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrayPostData));
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+      $result = curl_exec($ch);
+      curl_close ($ch);
+   }
+   exit;
 
-require "vendor/autoload.php";
-
-$access_token = 'IKtQS7iWe72sF7u0/a6PpFVxuuiXnZ0Nc864Q8wd+U7dRaqpMjLeRXk/8A1QWQAHQLqPuTQj2XC8hWvjJDV5XMvtIlClUpSj0DPKuepgqJsWHwRFl9BHBrvxP6g9PVWnVJyMra6tTGOZK9wWrPP0rwdB04t89/1O/w1cDnyilFU=';
-
-$channelSecret = '51dde56e498eb4f015f49b76d3726334';
-
-$pushID = 'Ud4d744beed535713bb7f605ca0e1eba6';
-
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
-
-$image_link = 'http://youant.net/green/images/logo-default-196x47.png';
-$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($image_link);
-$response = $bot->pushMessage($pushID, $textMessageBuilder);
-
-echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
-
-
-
-
-
-
-
+?>

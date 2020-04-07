23297bbdb8bd9e3a194063886074def2764c6cba
armdshos <arm.panu@gmail.com>
2020-04-07 13:32:15 +0700

    Update botpush_db.php

--- botpush_db.php
+++ botpush_db.php
@@ -13,7 +13,7 @@ $pushID = 'Ud4d744beed535713bb7f605ca0e1eba6';
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
 
-$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('เชื่อมฐาน database !');
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('เชื่อมฐาน database : connection success.. ');
 $response = $bot->pushMessage($pushID, $textMessageBuilder);
 
 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
